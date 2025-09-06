5321ad89e370feb1e06d7ba74db4b11fece249f4
Cheemx <chinmaymahajan999@gmail.com>
2025-09-06 06:59:40 +0000

    created login handler!

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/Cheemx/gator/internal/config"
+	"github.com/Cheemx/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -12,21 +15,25 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
+	db, err := sql.Open("postgres", conf.DBURL)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	dbQueries := database.New(db)
 	st := state{
+		db:  dbQueries,
 		cfg: &conf,
 	}
+
 	cmds := commands{
 		cmdFuncMap: make(map[string]func(*state, command) error),
 	}
+
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
 	args := os.Args
-	if len(args) < 2 {
-		log.Println("the application expects at least 2 arguments for command and username")
-		os.Exit(1)
-	}
-	if len(args) < 3 {
-		log.Println("username is required")
-		os.Exit(1)
-	}
 	cmds.run(&st, command{name: args[1], args: args[1:]})
 }
--- state.go
+++ state.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/Cheemx/gator/internal/config"
+	"github.com/Cheemx/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,13 +23,36 @@ type command struct {
 }
 
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return errors.New("login handler expects a string argument")
+	if len(cmd.args) < 2 {
+		log.Println("username is required")
+		os.Exit(1)
 	}
-	err := s.cfg.SetUser(cmd.args[1])
+	_, err := s.db.GetUser(context.Background(), cmd.args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.cfg.SetUser(cmd.args[1])
 	if err != nil {
 		return err
 	}
 	fmt.Printf("User has been set to %s\n", cmd.args[1])
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		log.Fatal("username is required")
+	}
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[1],
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.cfg.SetUser(user.Name)
+	fmt.Printf("User has been created %+v\n", user)
 	return nil
 }
